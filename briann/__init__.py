d881501b8a0bfc7b3b9d64e511ae365ac57634a6
Lee Hart <lee.hart@well.ox.ac.uk>
2017-01-23 17:11:55 +0000

    Add arg functions

--- briann/__init__.py
+++ briann/__init__.py
@@ -1,2 +1,23 @@
+def egg():
+    return ('EGG')
+
+def bacon():
+    return ('BACON')
+    
+def sausage():
+    return ('SAUSAGE')
+    
 def spam():
     return ('SPAM')
+
+def beans():
+    return ('BEANS')
+
+def lobster():
+    return ('LOBSTER')
+    
+def crevettes():
+    return ('CREVETTES')
+    
+def pate():
+    return ('pâté')
--- briann/cli.py
+++ briann/cli.py
@@ -1,4 +1,20 @@
-import briann
+import briann, sys
 
 def main():
-    print briann.spam()
+    for arg in sys.argv:
+        if arg == 'egg':
+            print briann.egg()
+        elif arg == 'bacon':
+            print briann.bacon()
+        elif arg == 'sausage':
+            print briann.sausage()
+        elif arg == 'spam':
+            print briann.spam()
+        elif arg == 'beans':
+            print briann.beans()
+        elif arg == 'lobster':
+            print briann.lobster()
+        elif arg == 'crevettes':
+            print briann.crevettes()
+        elif arg == 'pâté':
+            print briann.pate()
